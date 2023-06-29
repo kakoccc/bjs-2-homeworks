8568ecb36bab699154931eb87a213567e96aef7c
Сергей Нехайчик <byrbka@yandex.ru>
2023-06-29 20:39:14 +0100

    Реализованно первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = Math.pow(b, 2) - 4 * a * c;
+  let x = (-b + Math.sqrt(d)) / (2 * a);
+  let y = (-b - Math.sqrt(d)) / (2 * a);
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
+    x = -b / (2 * a);
+    arr.push(x);
+  } else {
+    arr.push(x, y);
+  }  
   return arr;
-}
+ }
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+    percent = percent / 100 / 12;
+  let loanAmount = amount - contribution;
+  let payment = loanAmount * (percent + (percent / (((1 + percent)**countMonths) - 1)));
+  let sum = +(payment * countMonths).toFixed(2);
+  return sum;
 }
