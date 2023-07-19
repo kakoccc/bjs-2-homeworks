10cb19d80859d598432a76f9413dca71db973f24
Сергей Нехайчик <byrbka@yandex.ru>
2023-07-19 11:06:00 +0100

    3 - тье Задание выполнено полностью

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,8 @@
 function compareArrays(arr1, arr2) {
-  
+ return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 }
-
-function getUsersNamesInAgeRange(users, gender) {
-  
+ 
+function getUsersNamesInAgeRange(user, gender) {
+    return user.filter(user => user.gender === gender).map(filteredUser => filteredUser.age)
+    .reduce((accumulator, age, index, aserAgesArray) => accumulator + age / aserAgesArray.length, 0);  
 }
