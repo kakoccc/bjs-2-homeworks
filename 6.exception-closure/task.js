48101c45ed3e5cb7c93bb160d9c7c18800970ed9
Сергей Нехайчик <byrbka@yandex.ru>
2023-07-23 18:41:09 +0100

    6 - тое Задание выполнено полностью

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,56 @@
-﻿
+﻿function parseCount (value) {
+    const number = Number.parseFloat(value);
+    if (isNaN (number)) {
+        throw new Error ('Невалидное значение');        
+    } 
+    return number;
+}     
+
+function validateCount (value) {
+    try {
+        return parseCount (value);
+    }
+    catch (Error) {
+        return Error;
+    }
+}
+
+
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    
+        if (a + b < c || a + c < b || b + c < a) {
+           throw new Error ('Треугольник с такими сторонами не существует');
+        }       
+    }
+    get perimeter() {
+        return (this.a + this.b + this.c);
+    }
+    get area() {
+        const p = this.perimeter / 2;
+        const S = Number(Math.sqrt(p*(p-this.a)*(p-this.b)*(p-this.c)).toFixed(3));
+        return S;
+    }
+    
+}
+
+function getTriangle(a, b, c) {
+    try {
+      return new Triangle(a, b, c);
+    } catch (error) {
+      return {
+        get area() {
+          return "Ошибка! Треугольник не существует"
+        },
+        get perimeter() {
+          return "Ошибка! Треугольник не существует"
+        }
+      }
+    }
+  }
+
+
